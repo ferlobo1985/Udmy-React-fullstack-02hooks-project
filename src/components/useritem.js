64536f8598c90c405fe1b577f6a5c08d95540a4f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-24 16:48:05 -0300

    context part 2

--- src/components/useritem.js
+++ src/components/useritem.js
@@ -4,10 +4,22 @@ import { MyContext } from '../context';
 const UserItem = () => {
     const context = useContext(MyContext);
 
-    console.log(context)
     return(
        <>
-            User item
+           { context.active ?
+               context.users.map((item)=>(
+                    <div key={item.id}>
+                        <div>Name: {item.name} </div>
+                    </div>
+               ))
+           :null
+           }
+           <hr/>
+           <button
+            onClick={context.toggleActive}
+           >
+               Toggle it
+           </button>
        </>
     )
 }
